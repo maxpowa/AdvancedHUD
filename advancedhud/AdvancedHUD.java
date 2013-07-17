a6cc2ac1839bb7cdb17bcbef7c1d2ea13504e37b
Max <maxpowa1@gmail.com>
2013-07-17 09:32:20 -0600

    HudItem Registration, it's super easy

--- advancedhud/AdvancedHUD.java
+++ advancedhud/AdvancedHUD.java
@@ -1,19 +1,19 @@
 package advancedhud;
 
-import java.io.File;
-import java.util.logging.Logger;
-
 import advancedhud.api.HUDRegistry;
-import advancedhud.api.HudItem;
-import advancedhud.api.SaveController;
+import advancedhud.client.huditems.HudItemAir;
+import advancedhud.client.huditems.HudItemArmor;
+import advancedhud.client.huditems.HudItemBossBar;
+import advancedhud.client.huditems.HudItemExperienceBar;
+import advancedhud.client.huditems.HudItemFood;
 import advancedhud.client.huditems.HudItemHealth;
+import advancedhud.client.huditems.HudItemHealthMount;
 import advancedhud.client.huditems.HudItemHotbar;
-
+import advancedhud.client.huditems.HudItemJumpBar;
 import cpw.mods.fml.client.registry.KeyBindingRegistry;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.registry.TickRegistry;
 import cpw.mods.fml.relauncher.Side;
 
@@ -22,28 +22,24 @@ public class AdvancedHUD {
 
     public static String MC_VERSION = "@MCVERSION@";
     public static String ADVHUD_VERSION = "@VERSION@";
-    
-    public static File configFile = null;
-    
+
     @EventHandler
     public void onInit(FMLInitializationEvent event) {
-        System.out.println("Loaded config file: " + configFile.getAbsolutePath());
         KeyBindingRegistry.registerKeyBinding(new KeyRegister());
         TickRegistry.registerTickHandler(new TickHandler(), Side.CLIENT);
-        
+
         registerHUDItems();
     }
-    
-    @EventHandler
-    public void onPostInit(FMLPostInitializationEvent event) {
-        if (!SaveController.loadConfig("config")) {
-            ;
-            SaveController.saveConfig("config");
-        }
-    }
-    
+
     private void registerHUDItems() {
         HUDRegistry.registerHudItem(new HudItemHotbar());
         HUDRegistry.registerHudItem(new HudItemHealth());
-    }    
+        HUDRegistry.registerHudItem(new HudItemAir());
+        HUDRegistry.registerHudItem(new HudItemFood());
+        HUDRegistry.registerHudItem(new HudItemArmor());
+        HUDRegistry.registerHudItem(new HudItemBossBar());
+        HUDRegistry.registerHudItem(new HudItemJumpBar());
+        HUDRegistry.registerHudItem(new HudItemHealthMount());
+        HUDRegistry.registerHudItem(new HudItemExperienceBar());
+    }
 }
