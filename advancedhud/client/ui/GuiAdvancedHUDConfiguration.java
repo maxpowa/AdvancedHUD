52f4312e39c752f3b09d3a791f405a87c97da0ff
Max <maxpowa1@gmail.com>
2013-07-17 09:32:04 -0600

    Still working on this
    Trying to build a system where it's possible to switch from mount HUD to
    player HUD at the touch of a button. (In config)

--- advancedhud/client/ui/GuiAdvancedHUDConfiguration.java
+++ advancedhud/client/ui/GuiAdvancedHUDConfiguration.java
@@ -1,15 +1,18 @@
 package advancedhud.client.ui;
 
+import org.lwjgl.input.Keyboard;
+
+import advancedhud.SaveController;
 import advancedhud.api.HUDRegistry;
 import advancedhud.api.HudItem;
-import advancedhud.api.SaveController;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.ScaledResolution;
 
 public class GuiAdvancedHUDConfiguration extends GuiScreen {
 
+    private static boolean asMount = false;
+    
     @Override
     public void initGui() {
         super.initGui();
@@ -19,20 +22,22 @@ public class GuiAdvancedHUDConfiguration extends GuiScreen {
     @SuppressWarnings("unchecked")
     private void addButtons() {
         this.buttonList.clear();
-        for (HudItem huditem : HUDRegistry.getActiveHudItemList()) {
-            this.buttonList.add(new GuiHudItemButton(huditem.getDefaultID(), huditem.posX, huditem.posY, huditem.getWidth(), huditem.getHeight(), huditem.getButtonLabel()));
+        for (HudItem huditem : HUDRegistry.getHudItemList()) {
+            if (asMount && huditem.isDrawnWhenOnMount()) {
+                this.buttonList.add(new GuiHudItemButton(huditem.getDefaultID(), huditem.posX, huditem.posY, huditem.getWidth(), huditem.getHeight(), huditem.getButtonLabel()));
+            } else if (!asMount) {
+                this.buttonList.add(new GuiHudItemButton(huditem.getDefaultID(), huditem.posX, huditem.posY, huditem.getWidth(), huditem.getHeight(), huditem.getButtonLabel()));
+            }
         }
     }
     
     @Override
     public void drawScreen(int par1, int par2, float par3) {
         this.drawDefaultBackground();
-        
-        ScaledResolution sr = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
-        int width = sr.getScaledWidth();
 
-        String s = "AdvancedHUD Config";
-        mc.fontRenderer.drawStringWithShadow(s, width / 2 - mc.fontRenderer.getStringWidth(s) / 2, 16, 16777215);
+        drawCenteredString(mc.fontRenderer, "LEFT CLICK to reposition, RIGHT CLICK to change settings", this.width/2, 17, 16777215);
+        drawCenteredString(mc.fontRenderer, "ESCAPE to cancel, R to reset all", this.width/2, 27, 16777215);
+        drawCenteredString(mc.fontRenderer, "M to change to" + (asMount ? " player " : " mount ") + "HUD", this.width/2, 37, 16777215);
         
         super.drawScreen(par1, par2, par3);
     }
@@ -44,11 +49,12 @@ public class GuiAdvancedHUDConfiguration extends GuiScreen {
     protected void keyTyped(char par1, int par2)
     {
         if (par2 == 19) {
-            for (HudItem huditem : HUDRegistry.getHudItemList()) {
-                huditem.alignment = huditem.getDefaultAlignment();
-                huditem.posX = huditem.getDefaultPosX();
-                huditem.posY = huditem.getDefaultPosY();
-            }
+            HUDRegistry.resetAllDefaults();
+            initGui();
+        }
+        if (par2 == Keyboard.KEY_M) {
+            asMount = !asMount;
+            initGui();
         }
         SaveController.saveConfig("config");
         super.keyTyped(par1, par2);
@@ -62,8 +68,6 @@ public class GuiAdvancedHUDConfiguration extends GuiScreen {
                 Minecraft.getMinecraft().displayGuiScreen(new GuiScreenReposition(this, hudItem));
             }
         }
-
-        SaveController.saveConfig("config");
         super.actionPerformed(par1GuiButton);
     }
     
