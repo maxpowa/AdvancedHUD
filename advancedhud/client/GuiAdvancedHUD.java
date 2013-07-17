f75fae323edad6a2e621111e15ed1e370b5537e3
Max <maxpowa1@gmail.com>
2013-07-17 09:30:05 -0600

    Removing code as I put it into HudItems

--- advancedhud/client/GuiAdvancedHUD.java
+++ advancedhud/client/GuiAdvancedHUD.java
@@ -1,9 +1,5 @@
 package advancedhud.client;
 
-import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.AIR;
-import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.ARMOR;
-import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.EXPERIENCE;
-import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.FOOD;
 import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.HEALTHMOUNT;
 import static net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType.JUMPBAR;
 
@@ -11,21 +7,17 @@ import org.lwjgl.opengl.GL11;
 
 import advancedhud.api.HUDRegistry;
 import advancedhud.api.HudItem;
-import net.minecraft.block.material.Material;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.potion.Potion;
 import net.minecraft.profiler.Profiler;
-import net.minecraft.util.FoodStats;
-import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.GuiIngameForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
-import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.common.MinecraftForge;
 
 public class GuiAdvancedHUD extends GuiIngameForge {
@@ -60,179 +52,29 @@ public class GuiAdvancedHUD extends GuiIngameForge {
         eventParent = new RenderGameOverlayEvent(partialTicks, res, mouseX, mouseY);
         
         for (HudItem huditem : HUDRegistry.getHudItemList()) {
-            huditem.fixBounds();
-            huditem.render(partialTicks);
-            //System.out.println("Rendered "+huditem.getButtonLabel());
-            //System.out.println("At "+huditem.posX+","+huditem.posY);
+            if (mc.thePlayer.ridingEntity instanceof EntityLivingBase) {
+                if (huditem.isDrawnWhenOnMount()) {
+                    huditem.fixBounds();
+                    huditem.render(partialTicks);
+                }
+            } else {
+                huditem.fixBounds();
+                huditem.render(partialTicks);
+            }
         }
-        //super.renderGameOverlay(partialTicks, hasScreen, mouseX, mouseY);
+        profiler.startSection("crosshair");
+        bind(Gui.field_110324_m);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_ONE_MINUS_DST_COLOR, GL11.GL_ONE_MINUS_SRC_COLOR);
+        drawTexturedModalRect(HUDRegistry.screenWidth / 2 - 7, HUDRegistry.screenHeight / 2 - 7, 0, 0, 16, 16);
+        GL11.glDisable(GL11.GL_BLEND);
+        profiler.endSection();
         profiler.endSection();
     }
 
     public ScaledResolution getResolution()
     {
         return res;
-    }
-
-    @Override
-    public void renderArmor(int width, int height)
-    {
-        if (pre(ARMOR)) return;
-        mc.mcProfiler.startSection("armor");
-
-        int left = width / 2 - 91;
-        int top = height - left_height;
-
-        int level = ForgeHooks.getTotalArmorValue(mc.thePlayer);
-        for (int i = 1; level > 0 && i < 20; i += 2)
-        {
-            if (i < level)
-            {
-                drawTexturedModalRect(left, top, 34, 9, 9, 9);
-            }
-            else if (i == level)
-            {
-                drawTexturedModalRect(left, top, 25, 9, 9, 9);
-            }
-            else if (i > level)
-            {
-                drawTexturedModalRect(left, top, 16, 9, 9, 9);
-            }
-            left += 8;
-        }
-        left_height += 10;
-
-        mc.mcProfiler.endSection();
-        post(ARMOR);
-    }
-    
-    @Override
-    public void renderAir(int width, int height)
-    {
-        if (pre(AIR)) return;
-        mc.mcProfiler.startSection("air");
-        int left = width / 2 + 91;
-        int top = height - right_height;
-
-        if (mc.thePlayer.isInsideOfMaterial(Material.water))
-        {
-            int air = mc.thePlayer.getAir();
-            int full = MathHelper.ceiling_double_int((double)(air - 2) * 10.0D / 300.0D);
-            int partial = MathHelper.ceiling_double_int((double)air * 10.0D / 300.0D) - full;
-
-            for (int i = 0; i < full + partial; ++i)
-            {
-                drawTexturedModalRect(left - i * 8 - 9, top, (i < full ? 16 : 25), 18, 9, 9);
-            }
-            right_height += 10;
-        }
-
-        mc.mcProfiler.endSection();
-        post(AIR);
-    }
-
-    public void renderFood(int width, int height)
-    {
-        if (pre(FOOD)) return;
-        mc.mcProfiler.startSection("food");
-
-        int left = width / 2 + 91;
-        int top = height - right_height;
-        boolean unused = false;// Unused flag in vanilla, seems to be part of a 'fade out' mechanic
-
-        FoodStats stats = mc.thePlayer.getFoodStats();
-        int level = stats.getFoodLevel();
-        int levelLast = stats.getPrevFoodLevel();
-
-        for (int i = 0; i < 10; ++i)
-        {
-            right_height = 10+30;
-            
-            int idx = i * 2 + 1;
-            int x = left - i * 8 - 9;
-            int y = top;
-            int icon = 16;
-            byte backgound = 0;
-
-            if (mc.thePlayer.isPotionActive(Potion.hunger))
-            {
-                icon += 36;
-                backgound = 13;
-            }
-            if (unused) backgound = 1; //Probably should be a += 1 but vanilla never uses this
-
-            if (mc.thePlayer.getFoodStats().getSaturationLevel() <= 0.0F && updateCounter % (level * 3 + 1) == 0)
-            {
-                y = top + (rand.nextInt(3) - 1);
-            }
-
-            drawTexturedModalRect(x, y, 16 + backgound * 9, 27, 9, 9);
-
-            if (unused)
-            {
-                if (idx < levelLast)
-                    drawTexturedModalRect(x, y, icon + 54, 27, 9, 9);
-                else if (idx == levelLast)
-                    drawTexturedModalRect(x, y, icon + 63, 27, 9, 9);
-            }
-
-            if (idx < level)
-                drawTexturedModalRect(x, y, icon + 36, 27, 9, 9);
-            else if (idx == level)
-                drawTexturedModalRect(x, y, icon + 45, 27, 9, 9);
-        }
-        mc.mcProfiler.endSection();
-        post(FOOD);
-    }
-
-    @Override
-    public void renderExperience(int width, int height)
-    {
-        bind(field_110324_m);
-        if (pre(EXPERIENCE)) return;
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        
-        if (mc.playerController.func_78763_f())
-        {
-            mc.mcProfiler.startSection("expBar");
-            int cap = this.mc.thePlayer.xpBarCap();
-            int left = width / 2 - 91;
-    
-            if (cap > 0)
-            {
-                short barWidth = 182;
-                int filled = (int)(mc.thePlayer.experience * (float)(barWidth + 1));
-                int top = height - 32 + 3;
-                drawTexturedModalRect(left, top, 0, 64, barWidth, 5);
-    
-                if (filled > 0)
-                {
-                    drawTexturedModalRect(left, top, 0, 69, filled, 5);
-                }
-            }
-
-            this.mc.mcProfiler.endSection();
-            
-        
-            if (mc.playerController.func_78763_f() && mc.thePlayer.experienceLevel > 0)
-            {
-                mc.mcProfiler.startSection("expLevel");
-                boolean flag1 = false;
-                int color = flag1 ? 16777215 : 8453920;
-                String text = "" + mc.thePlayer.experienceLevel;
-                int x = (width - mc.fontRenderer.getStringWidth(text)) / 2;
-                int y = height - 31 - 4;
-                mc.fontRenderer.drawString(text, x + 1, y, 0);
-                mc.fontRenderer.drawString(text, x - 1, y, 0);
-                mc.fontRenderer.drawString(text, x, y + 1, 0);
-                mc.fontRenderer.drawString(text, x, y - 1, 0);
-                mc.fontRenderer.drawString(text, x, y, color);
-                mc.mcProfiler.endSection();
-            }
-        }
-        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-
-        post(EXPERIENCE);
     }
 
     @Override
