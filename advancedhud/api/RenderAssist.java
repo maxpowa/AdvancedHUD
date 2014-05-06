868e88eef4bc7b47523afaca7ca8781ea569f68f
maxpowa <maxpowa1@gmail.com>
2014-05-06 13:46:30 -0600

    Update RenderAssist
    Adds some new useful methods (filled/unfilled circles, filled/unfilled
    rectangles)

--- advancedhud/api/RenderAssist.java
+++ advancedhud/api/RenderAssist.java
@@ -1,12 +1,12 @@
 package advancedhud.api;
 
-import org.lwjgl.opengl.GL11;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
 
 /**
  * 
@@ -22,6 +22,123 @@ public class RenderAssist {
      * Controls render "level" for layering textures overtop one another.
      */
     public static float zLevel;
+
+    public static void drawUnfilledCircle(float posX, float posY, float radius, int num_segments, int color) {
+        float f = (color >> 24 & 255) / 255.0F;
+        float f1 = (color >> 16 & 255) / 255.0F;
+        float f2 = (color >> 8 & 255) / 255.0F;
+        float f3 = (color & 255) / 255.0F;
+        Tessellator tessellator = Tessellator.instance;
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(f1, f2, f3, f);
+        tessellator.startDrawing(GL11.GL_LINE_LOOP);
+        for (int i = 0; i < num_segments; i++) {
+            double theta = 2.0f * Math.PI * i / num_segments;// get the current
+                                                             // angle
+
+            double x = radius * Math.cos(theta);// calculate the x component
+            double y = radius * Math.sin(theta);// calculate the y component
+
+            tessellator.addVertex(x + posX, y + posY, 0.0D);// output vertex
+
+        }
+        tessellator.draw();
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
+
+    public static void drawCircle(float posX, float posY, float radius, int num_segments, int color) {
+        float f = (color >> 24 & 255) / 255.0F;
+        float f1 = (color >> 16 & 255) / 255.0F;
+        float f2 = (color >> 8 & 255) / 255.0F;
+        float f3 = (color & 255) / 255.0F;
+        // Tessellator tessellator = Tessellator.instance;
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        // GL11.glShadeModel(GL11.GL_SMOOTH);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(f1, f2, f3, f);
+        
+        GL11.glBegin(GL11.GL_TRIANGLE_FAN);
+        GL11.glVertex2f(posX, posY); // center of circle
+        for (int i = num_segments; i >= 0; i--) {
+            double theta = i * (Math.PI*2) / num_segments;
+            GL11.glVertex2d(posX + radius * Math.cos(theta), posY + radius * Math.sin(theta));
+        }
+        GL11.glEnd();
+        
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
+
+    /**
+     * Draws a color rectangle outline with the specified coordinates and color.<br>
+     * Color must have all four hex elements (0xFFFFFFFF)
+     * 
+     * @param x1
+     *            First X value
+     * @param g
+     *            First Y value
+     * @param x2
+     *            Second X value
+     * @param y2
+     *            Second Y Value
+     */
+    public static void drawUnfilledRect(float x1, float g, float x2, float y2, int color) {
+        float j1;
+
+        if (x1 < x2) {
+            j1 = x1;
+            x1 = x2;
+            x2 = j1;
+        }
+
+        if (g < y2) {
+            j1 = g;
+            g = y2;
+            y2 = j1;
+        }
+
+        float f = (color >> 24 & 255) / 255.0F;
+        float f1 = (color >> 16 & 255) / 255.0F;
+        float f2 = (color >> 8 & 255) / 255.0F;
+        float f3 = (color & 255) / 255.0F;
+        Tessellator tessellator = Tessellator.instance;
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor4f(f1, f2, f3, f);
+        tessellator.startDrawing(GL11.GL_LINE_LOOP);
+        tessellator.addVertex(x1, y2, 0.0D);
+        tessellator.addVertex(x2, y2, 0.0D);
+        tessellator.addVertex(x2, g, 0.0D);
+        tessellator.addVertex(x1, g, 0.0D);
+        tessellator.draw();
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_BLEND);
+    }
+
+    public static void drawHorizontalLine(int par1, int par2, int par3, int par4) {
+        if (par2 < par1) {
+            int i1 = par1;
+            par1 = par2;
+            par2 = i1;
+        }
+
+        drawRect(par1, par3, par2 + 1, par3 + 1, par4);
+    }
+
+    public static void drawVerticalLine(int par1, int par2, int par3, int par4) {
+        if (par3 < par2) {
+            int i1 = par2;
+            par2 = par3;
+            par3 = i1;
+        }
+
+        drawRect(par1, par2 + 1, par1 + 1, par3, par4);
+    }
 
     /**
      * Draws a textured rectangle at the stored z-value.
@@ -39,19 +156,14 @@ public class RenderAssist {
      * @param height
      *            Height to render the sprite.
      */
-    public static void drawTexturedModalRect(int x, int y, int u, int v,
-            int width, int height) {
+    public static void drawTexturedModalRect(float x, float y, float u, float v, float width, float height) {
         float f = 0.00390625F;
         Tessellator tessellator = Tessellator.instance;
         tessellator.startDrawingQuads();
-        tessellator.addVertexWithUV(x + 0, y + height, RenderAssist.zLevel,
-                (u + 0) * f, (v + height) * f);
-        tessellator.addVertexWithUV(x + width, y + height, RenderAssist.zLevel,
-                (u + width) * f, (v + height) * f);
-        tessellator.addVertexWithUV(x + width, y + 0, RenderAssist.zLevel,
-                (u + width) * f, (v + 0) * f);
-        tessellator.addVertexWithUV(x + 0, y + 0, RenderAssist.zLevel, (u + 0)
-                * f, (v + 0) * f);
+        tessellator.addVertexWithUV(x + 0, y + height, RenderAssist.zLevel, (u + 0) * f, (v + height) * f);
+        tessellator.addVertexWithUV(x + width, y + height, RenderAssist.zLevel, (u + width) * f, (v + height) * f);
+        tessellator.addVertexWithUV(x + width, y + 0, RenderAssist.zLevel, (u + width) * f, (v + 0) * f);
+        tessellator.addVertexWithUV(x + 0, y + 0, RenderAssist.zLevel, (u + 0) * f, (v + 0) * f);
         tessellator.draw();
     }
 
@@ -70,47 +182,48 @@ public class RenderAssist {
         ResourceLocation res = new ResourceLocation(textureLocation);
         Minecraft.getMinecraft().getTextureManager().bindTexture(res);
     }
-    
+
     /**
      * Draws a solid color rectangle with the specified coordinates and color.
      * 
-     * @param x1    First X value
-     * @param x2    Second X value
-     * @param y1    First Y value
-     * @param y2    Second Y Value
+     * @param g
+     *            First X value
+     * @param h
+     *            First Y value
+     * @param i
+     *            Second X value
+     * @param j
+     *            Second Y Value
      */
-    public static void drawRect(int x1, int y1, int x2, int y2, int color)
-    {
-        int j1;
+    public static void drawRect(float g, float h, float i, float j, int color) {
+        float j1;
 
-        if (x1 < x2)
-        {
-            j1 = x1;
-            x1 = x2;
-            x2 = j1;
+        if (g < i) {
+            j1 = g;
+            g = i;
+            i = j1;
         }
 
-        if (y1 < y2)
-        {
-            j1 = y1;
-            y1 = y2;
-            y2 = j1;
+        if (h < j) {
+            j1 = h;
+            h = j;
+            j = j1;
         }
 
-        float f = (float)(color >> 24 & 255) / 255.0F;
-        float f1 = (float)(color >> 16 & 255) / 255.0F;
-        float f2 = (float)(color >> 8 & 255) / 255.0F;
-        float f3 = (float)(color & 255) / 255.0F;
+        float f = (color >> 24 & 255) / 255.0F;
+        float f1 = (color >> 16 & 255) / 255.0F;
+        float f2 = (color >> 8 & 255) / 255.0F;
+        float f3 = (color & 255) / 255.0F;
         Tessellator tessellator = Tessellator.instance;
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         GL11.glColor4f(f1, f2, f3, f);
         tessellator.startDrawingQuads();
-        tessellator.addVertex((double)x1, (double)y2, 0.0D);
-        tessellator.addVertex((double)x2, (double)y2, 0.0D);
-        tessellator.addVertex((double)x2, (double)y1, 0.0D);
-        tessellator.addVertex((double)x1, (double)y1, 0.0D);
+        tessellator.addVertex(g, j, 0.0D);
+        tessellator.addVertex(i, j, 0.0D);
+        tessellator.addVertex(i, h, 0.0D);
+        tessellator.addVertex(g, h, 0.0D);
         tessellator.draw();
         GL11.glEnable(GL11.GL_TEXTURE_2D);
         GL11.glDisable(GL11.GL_BLEND);
@@ -120,8 +233,7 @@ public class RenderAssist {
      * Renders the specified item of the inventory slot at the specified
      * location.
      */
-    public static void renderInventorySlot(int slot, int x, int y,
-            float partialTick, Minecraft mc) {
+    public static void renderInventorySlot(int slot, int x, int y, float partialTick, Minecraft mc) {
         RenderItem itemRenderer = new RenderItem();
         ItemStack itemstack = mc.thePlayer.inventory.mainInventory[slot];
         x += 91;
@@ -138,15 +250,13 @@ public class RenderAssist {
                 GL11.glTranslatef(-(x + 8), -(y + 12), 0.0F);
             }
 
-            itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer,
-                    mc.getTextureManager(), itemstack, x, y);
+            itemRenderer.renderItemAndEffectIntoGUI(mc.fontRenderer, mc.getTextureManager(), itemstack, x, y);
 
             if (f1 > 0.0F) {
                 GL11.glPopMatrix();
             }
 
-            itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer,
-                    mc.getTextureManager(), itemstack, x, y);
+            itemRenderer.renderItemOverlayIntoGUI(mc.fontRenderer, mc.getTextureManager(), itemstack, x, y);
         }
     }
 
